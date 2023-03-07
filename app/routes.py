b58c40ed9017ebff31c5376c83e3d1f36a596a13
paulformoney <1018690908@qq.com>
2023-03-07 21:16:00 +0800

    1.模板使用及继承

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,22 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello,world"
+    user = {"username":"paul"}
+    # 列表存储两个字典，代表两个用户与其发布的微博
+    posts = [
+        {'author':{'username':'uzi'},
+         'body':'薇恩'
+         },
+        {
+            'author':{'username':'rookie'},
+            'body':'发条'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user,posts=posts)
 
 
 @app.route('/user/<name>')
